afd2a2776d9d960415a1ec72ff2915583c0a5e27
Kadu <ce.magalhaespereira@gmail.com>
2022-07-20 18:44:54 -0300

    Add TaskInput form validation

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -2,15 +2,45 @@ import styles from "./TaskInput.module.css"
 
 //assets
 import CreateIcon from "../assets/CreateIcon.svg"
+import React, { useState } from "react"
 
 export function TaskInput() {
+
+    const [newTask, setNewTask] = useState("")
+
+    const handleNewTaskInvalid = (event: React.InvalidEvent<HTMLInputElement>): void => {
+        event.target.setCustomValidity('Este campo não pode ser vazio!')
+    }
+
+    const handleNewTaskChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        event.target.setCustomValidity('')
+        setNewTask(event?.target.value)
+    }
+    console.log(newTask)
+
+    const isNewTaskEmpty = newTask === "" 
+
     return (
-        <div className={styles.wrapper}>
-            <input className={styles.input} type="text" placeholder="Adicione uma nova tarefa" autoFocus />
-            <button className={styles.button}>
+        <form 
+        className={styles.wrapper}
+        >
+            <input 
+                className={styles.input} 
+                type="text" 
+                placeholder="Adicione uma nova tarefa" 
+                onChange={handleNewTaskChange}
+                onInvalid={handleNewTaskInvalid}
+                autoFocus 
+                required
+            />
+            <button 
+                type="submit"
+                className={styles.button} 
+                disabled={isNewTaskEmpty}
+            >
                 Criar
                 <img src={CreateIcon} alt="Criar" />
             </button>
-        </div>
+        </form>
     )
 }
